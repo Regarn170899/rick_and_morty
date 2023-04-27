2e0d8622b4b2dd0bfa926cad4f80adc074d21a96
Regarn170899 <1526497@gmail.com>
2023-04-27 16:22:08 +0200

    reset head to main

--- src/components/RouterConfig/RouterConfig.tsx
+++ src/components/RouterConfig/RouterConfig.tsx
@@ -4,18 +4,24 @@ import Loader from '@pages/Loader/Loader';
 import Layout from "../Layout/Layout";
 import Home from "@pages/Home/Home";
 import {PathRouter} from "@constants/index";
+import SingleCharacter from "@pages/SingleCharacter/SingleCharacter";
 
-    const isLoading:boolean=true;
+
+
 
 const RouterConfig = () : ReactElement => {
-    const [isLoading,setLoading]=useState(true)
+    const [isLoading,setLoading]=useState<boolean>(false)
     return (
         <>
             <React.Suspense fallback={<Loader/>}>
                 <Routes>
+
                     <Route path={PathRouter.Home}
-                        element={<Layout>{isLoading ? <Home />: <Outlet/> }</Layout>}
-                    />
+                        element={<Layout>{isLoading ? <Loader/>: <Outlet/> }</Layout>}
+                    >
+                        <Route path="/:id" element={<SingleCharacter/>} />
+                        <Route path={PathRouter.Home} element={<Home/>} />
+                    </Route>
                 </Routes>
             </React.Suspense>
         </>
--- src/store/slices/charactersSlice.ts
+++ src/store/slices/charactersSlice.ts
@@ -1,26 +1,34 @@
 import {createAsyncThunk, createSlice, PayloadAction,} from "@reduxjs/toolkit";
 import { RootState } from "store/index";
-import {getApiCharacters} from "api";
+import {getApiCharacters, getApiSingleCharacters} from "api";
 import {RMCharacter}from "@customTypes/index";
-import { setIsFavoriteProp} from "@utils/index";
+import {checkIsFavorite, setIsFavoriteProp} from "@utils/index";
 
 
 interface CharactersState {
     status: string;
     charactersList: RMCharacter[];
     page: number;
+    currentChar:RMCharacter |null;
 }
 
 const initialState: CharactersState = {
     status: "",
     charactersList: [],
     page: 1,
+    currentChar:null,
 };
 export const getCharacters = createAsyncThunk(
     "characters/getCharacters",
     async (page: number) => {
         const {results} = await getApiCharacters(page);
         return setIsFavoriteProp(results);
+    });
+export const getCurrentChar = createAsyncThunk(
+    "CurrentChar/getCurrentChar",
+    async (id: string) => {
+        const character = await getApiSingleCharacters(id);
+        return checkIsFavorite(character);
     });
 
 export const charactersSlice = createSlice({
@@ -38,11 +46,17 @@ export const charactersSlice = createSlice({
                 return character;
             })
         },
+        changeIsFavoriteCurrentChar(state){
+            if(state.currentChar===null)return;
+            state.currentChar={...state.currentChar,isFavorite:!state.currentChar.isFavorite};
+        },
         setPage(state,action:PayloadAction<number>){
             state.page=action.payload;
+        },
+        setCurrentChar(state,action:PayloadAction<RMCharacter>){
+            state.currentChar=action.payload;
         }
     },
-
     extraReducers: (builder) => {
         builder
             .addCase(getCharacters.pending, (state) => {
@@ -55,14 +69,29 @@ export const charactersSlice = createSlice({
                     return
                 }
                 state.charactersList = [...state.charactersList, ...results];
-            });
+            })
+            .addCase(getCurrentChar.pending, (state) => {
+                state.status = "loading";
+            })
+
+            .addCase(getCurrentChar.fulfilled, (state, action) => {
+                state.status = "success";
+                state.currentChar =  action.payload;
+            })
     },
+
+
+
+
+
 });
 
 export const selectCharacters = (state: RootState) =>
     state.characters.charactersList;
+export const selectCurrentChar = (state: RootState) =>
+    state.characters.currentChar;
 
-export const {updateFavorites,setPage}=charactersSlice.actions
+export const {updateFavorites,setPage,setCurrentChar,changeIsFavoriteCurrentChar}=charactersSlice.actions
 
 export const selectPage = (state: RootState) =>
     state.characters.page;
